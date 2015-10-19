577d22321479c5fbc6bd5e559d44c22d305ef2ca
ZhuNian <zhunianhhxy@163.com>
2015-10-19 22:41:54 +0800

    float number in js

--- js_try.js
+++ js_try.js
@@ -1,5 +1,14 @@
 function load(){
-	test();
+	number_test();
+}
+function number_test(){
+	//js使用“二进制浮点数表示法”，可以精确的表示1/2，1/8,1/1024这种分数，但是对于其他
+	//的分数就不行，比如说1/10；实际上可以看到0.3-0.2结果却是等于0.0999999...的
+	var x = .3-.2;
+	var y=.2-.1;
+	console.log(x);
+	console.log(y);
+	console.log(x==y);
 }
 function test(){
 	var points = [
@@ -7,7 +16,7 @@ function test(){
 		{x:1,y:1}
 	];
 	points.dist = function(){
-		return 1;
+		return 1;//把数组理解成为一个对象就可以理解了
 	}
 	console.log(points);
 }
