9e2a6149d214ec337d80f1df283545960d437c34
ZhuNian <zhunianhhxy@163.com>
2015-10-22 18:15:58 +0800

    use js to store cookie

--- js_try.js
+++ js_try.js
@@ -1,5 +1,19 @@
 function load(){
-	number_test();
+	cookie_test();
+	console.log(getCookie('username'));
+}
+function cookie_test(){
+	document.cookie='username=ZN';
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
 }
 function number_test(){
 	//js使用“二进制浮点数表示法”，可以精确的表示1/2，1/8,1/1024这种分数，但是对于其他
